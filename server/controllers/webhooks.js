9247766f3ff30488ef8a734c56e4a428877ca004
Vishal Verma <vishalverma1514@gmail.com>
2026-05-31 16:31:14 +0530

    corrected user creation

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -12,8 +12,10 @@ export const clerkWebhooks= async (req, res)=>{
             "svix-timestamp" : req.headers["svix-timestamp"],
             "svix-signature" : req.headers["svix-signature"]
         })
-
+        
         const {data, type} = req.body
+
+       
 
         switch (type) {
             case 'user.created': {
@@ -30,7 +32,7 @@ export const clerkWebhooks= async (req, res)=>{
 
             case 'user.updated': {
                 const userData = {
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
                 }
