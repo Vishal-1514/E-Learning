bed98d984d76105c3e2d8ea385a0b25c0e1134c6
Vishal Verma <vishalverma1514@gmail.com>
2026-06-17 16:08:03 +0530

    api key updated

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
               }
             </div>
             { user ? <UserButton/> :
-            <button onClick={()=> openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
+            <button onClick={()=> openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full cursor-pointer'>Create Account</button>
             }
         </div>
         {/* For Phone Screens */}
--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -4,6 +4,6 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
     mongoose.connection.on('connected', ()=> console.log('database connected'))
-    await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
+    await mongoose.connect(`${process.env.MONGODB_URL}/lms`)
 }
 export default connectDB
